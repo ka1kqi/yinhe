d228ff5bb9f6b9db137d5e8252ceee265c5ac677
Arthur Du <arthur.x.du@gmail.com>
2024-10-21 03:22:30 -0400

    create logger for orderbook functions

--- src/common/orderLog.hpp
+++ src/common/orderLog.hpp
@@ -7,26 +7,58 @@
  * Keeps track of the tick of the simulation as well, incremented by 1000
  * 
 */
+#include <string>
 #include <iostream>
 #include <fstream>
-#include "types.hpp"
-#include "enums.hpp"
+#include <filesystem>
+#include "order.hpp"
 #include "tradeUtils/trade.hpp"
 
 using Log = std::ofstream;
 
-Log logFile;
-SimTick curLogTick;
+class OrderbookLogger {
+public:
+    /*TAKES A DIRECTORY*/
+    void set_logfile_save_location(std::string filepath) {
+        /*eventually add checking to ensure that filepath is a valid directory*/
+        if(!std::filesystem::is_directory(filepath)) {
+            std::cerr << ("Invalid filepath: provided filepath is not a directory\n");
+            std::exit(1); /*fatal exception if logfile location is wrong*/
+        }
+        CUSTOM_LOGFILE_SAVE_LOCATION = filepath;
+    }
+    void init_Log(){
+        std::string logfile_location = CUSTOM_LOGFILE_SAVE_LOCATION.empty() ? DEFAULT_LOGFILE_SAVE_LOCATION : CUSTOM_LOGFILE_SAVE_LOCATION;
+        logFile = std::ofstream(logfile_location,std::ios::app);
+        curLogTick = 0;
+        std::cout << "Log file opened " << std::endl;
+        std::cout << "Tick: " << std::to_string(curLogTick) << std::endl;
+        logFile << "Initial log" << std::endl;
+    }
 
-void init_log(){
-    logFile = std::ofstream("log.txt",std::ios::app);
-    curLogTick = 0;
-    std::cout << "Log file opened " << std::endl;
-    std::cout << "Tick: " << std::to_string(curLogTick) << std::endl;
-}
+    /*TAKES TRADE POINTER*/
+    void log_Trade(Trade *trade_,SimTick simulation_tick_time){
+        tradeInfo *bid_trade = &(trade_->get_bid_info());
+        tradeInfo *ask_trade = &(trade_->get_ask_info());
+        logFile << std::to_string(simulation_tick_time) << " | " << std::to_string(ask_trade->orderID_) << " | " << 
+                    std::to_string(bid_trade->orderID_) << " | " << std::to_string(ask_trade->price_) << 
+                    " | " << std::to_string(ask_trade->quantity_) << std::endl;
+    }
 
-void log_Trade(Trade *trade_,SimTick tick){
-    
-}
+    void log_order_Error(OrderID err_order_id){
+        logFile << "Error with order: " << std::to_string(err_order_id);
+    }
 
+    void close_Log() {
+        logFile.close();
+    }
+
+private:
+    const std::string DEFAULT_LOGFILE_SAVE_LOCATION = "yinhe/docs/logs";
+    std::string CUSTOM_LOGFILE_SAVE_LOCATION;    
+    static Log logFile;
+    static SimTick curLogTick;
+};
+
+/*need to add mutex support eventually to prevent concurrent edits*/
 #endif
